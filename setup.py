573a628811144353eefd087a03726f6f27e55ad5
Jonas Baumann <jone@jone.ch>
2011-12-02 12:15:38 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -4,39 +4,48 @@ import os
 version = '1.0dev'
 maintainer = 'Thomas Buchberger'
 
-tests_require = ['collective.testcaselayer']
+tests_require = [
+    'collective.testcaselayer',
+    ]
 
 setup(name='ftw.quota',
       version=version,
-      description="Quota support for archetypes containers" + \
-          ' (Maintainer: %s)' % maintainer,
-      long_description=open("README.txt").read() + "\n" + \
-          open(os.path.join("docs", "HISTORY.txt")).read(),
+      description='Quota support for archetypes containers.',
+      long_description=open('README.txt').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         ],
-      keywords='ftw quota',
-      author='%s, 4teamwork GmbH' % maintainer,
+
+      keywords='ftw quota plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
-      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.quota/',
+      url='https://github.com/4teamwork/ftw.quota/',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
         'setuptools',
         'archetypes.schemaextender',
-        # -*- Extra requirements: -*-
         ],
+
       tests_require=tests_require,
       extras_require={'tests': tests_require},
-      entry_points="""
+      entry_points='''
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
-      """,
+      ''',
       )
