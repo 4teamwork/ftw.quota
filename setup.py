b7f9907169d56a193a76d888cfd19780fbc37ddd
Jonas Baumann <jone@jone.ch>
2011-12-02 12:16:43 +0100

    Renamed readme to .rst

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ tests_require = [
 setup(name='ftw.quota',
       version=version,
       description='Quota support for archetypes containers.',
-      long_description=open('README.txt').read() + '\n' + \
+      long_description=open('README.rst').read() + '\n' + \
           open(os.path.join('docs', 'HISTORY.txt')).read(),
 
       # Get more strings from
